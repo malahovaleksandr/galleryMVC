0cede8201042d0aed6ae1ce9df48a6c8a02e9c7d
“malahovaleksandr” <pro100boy@gmail.com>
2017-04-19 16:57:25 +0300

    start model and controller gallery

--- app/controllers/controller_gallery.php
+++ app/controllers/controller_gallery.php
@@ -4,11 +4,16 @@ class controller_gallery extends Controller {
     public function action_index()
     {
         $this->checkAyth();
-            $this->view->generate('gallery_view.twig',
-                array(
-                    'title'  => 'Галерея',
-                    'action' => 'controllers/controller_gallery'
-                ));
+        $saveData              = new Model_gallery();
+        //доделать запрос на выборку всех photo
+        $saveData->photo       = $this->incomeSrc;
+        $saveData->save();
+        $this->view->generate('gallery_view.twig',
+            array(
+                'title'  => 'Галерея',
+                'action' => '/gallery'
+            ));
+
     }
 
 
---
+++ app/models/model_gallery.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Model_gallery extends Model {
+
+    protected $table = 'dataUsers';
+    public $timestamps = false;
+
+
+    protected function get_data()
+    {
+
+    }
+
+}
+
+
